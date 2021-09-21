3151c4b07e61c451bef70419f1cc305ec653df22
Guilherme Matos de Carvalho <guilhermematos851@gmail.com>
2021-09-21 13:03:30 -0300

    starting js

---
+++ Urna/etapas.js
@@ -0,0 +1,31 @@
+let etapas = [
+    {
+        titulo: 'VEREADOR',
+        numeros: 5,
+        candidatos: [
+        {
+            numero: 21221,
+            name: 'Fulano de Tal',
+            partido: 'ABC',
+            fotos:[
+                {url: '21221.jpg', legenda: 'Vereador'}
+            ]
+        },
+        {
+            numero: 56982,
+            name: 'Beltrano da Silva',
+            partido: 'DEFG',
+            fotos:[
+                {url: '56982.png', legenda: 'Vereador'}
+            ]
+        },
+        ]
+    },
+    {
+        titulo: 'PREFEITO',
+        numeros: 2,
+        candidatos: [
+            
+        ]
+    }
+]
