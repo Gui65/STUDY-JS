a8be188ba74de11c5ff9b1ac0126de62bec3fc6c
Guilherme Matos de Carvalho <guilhermematos851@gmail.com>
2021-09-22 12:27:49 -0300

    modify script.js

--- Urna/js/script.js
+++ Urna/js/script.js
@@ -3,7 +3,26 @@ let cargo = document.querySelector('.d-1-2 span');
 let descricao = document.querySelector('.d-1-4');
 let aviso = document.querySelector('.d-2');
 let lateral = document.querySelector('.d-1-right');
-let numeros = document.querySelector('d-1-3');
+let numeros = document.querySelector('.d-1-3');
+
+let etapaAtual = 0;
+
+function comecarEtapa() {
+    let etapa = etapas[etapaAtual];
+
+    let numeroHtml = '';
+
+    for(let i=0;i<etapa.numeros;i++){
+        numeroHtml += '<div class="numero"></div>';
+    }
+
+    seuVotoPara.style.display = 'none';
+    cargo.innerHTML = etapa.titulo;
+    descricao.innerHTML = '';
+    aviso.style.display = 'none';
+    lateral.innerHTML = '';
+    numeros.innerHTML = numeroHtml;
+}
 
 function clicou(n){
     alert("Clicou em "+n);
@@ -17,3 +36,6 @@ function corrige(){
 function confirma(){
     alert("Clicou em CONFIRMA");
 }
+
+
+comecarEtapa();
